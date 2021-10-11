e3ccbed3e97753de783e513a051cdf027904a56e
Evgeny Obednin <lffblk@gmail.com>
2021-10-11 15:21:55 +0400

    Spring Boot application was configured

---
+++ src/main/kotlin/com/lffblk/stock/quality/StockQualityIndexApplication.kt
@@ -0,0 +1,11 @@
+package com.lffblk.stock.quality
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class StockQualityIndexApplication
+
+fun main(args: Array<String>) {
+	runApplication<StockQualityIndexApplication>(*args)
+}
